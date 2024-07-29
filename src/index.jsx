643e46ce049c07269e3610c649ca98cfa3bb4bef
ThEminent <lmukorode@gmail.com>
2024-07-29 12:06:41 +0100

    Update

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router }  from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import './index.css';
@@ -10,7 +10,7 @@ import { store } from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <Provider store={store}>
-      <Router basename="/Lyriks">
+      <Router>
         <App />
       </Router>
     </Provider>

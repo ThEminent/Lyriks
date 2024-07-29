8b39462dc5440cffe8c6765f788cd2b0fe4a7d60
ThEminent <lmukorode@gmail.com>
2024-07-29 12:10:33 +0100

    Update

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router }  from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import './index.css';
@@ -10,7 +10,7 @@ import { store } from './redux/store';
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router basename="/Lyriks">
         <App />
       </Router>
     </Provider>
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import ghPages from 'vite-plugin-gh-pages'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ghPages({
-      // This is the branch where your gh-pages site is built
-      branch: 'gh-pages',
-      // Your repository name
-      repo: 'https://github.com/theminent/Lyriks.git',
-    })
-  ],
+  plugins: [react()],
   base: '/Lyriks/',
-})
+});
